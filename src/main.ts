c2f45c6ac941d1b63bdbdacef4f6e574d6a0061f
Kartik Choudhary <kartik.c918@gmail.com>
2020-04-06 13:29:40 +0530

    hotfix #1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
