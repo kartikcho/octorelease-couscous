7eb018e9c031c27be29f6564967418df4c13a4c3
Kartik Choudhary <kartik.c918@gmail.com>
2020-04-06 15:47:53 +0530

    hotfix#2 (eslint-errors)

--- src/event.ts
+++ src/event.ts
@@ -1,12 +1,12 @@
 import * as gh from '@actions/github'
 import * as core from '@actions/core'
 
-export function getCreatedTag(): string | null{
-  if (gh.context.eventName != 'create') {
+export function getCreatedTag(): string | null {
+  if (gh.context.eventName !== 'create') {
     core.info('The event ${gh.context.eventName} occurred')
     return null
   }
-  if (gh.context.payload.ref_type != 'tag') {
+  if (gh.context.payload.ref_type !== 'tag') {
     core.info('The created reference was a branch, not a tag')
     return null
   }
--- src/github.ts
+++ src/github.ts
@@ -1,28 +1,31 @@
-import * as gh from "@actions/github"
+import * as gh from '@actions/github'
 import * as version from './version'
 import * as markdown from './markdown'
 import * as core from '@actions/core'
 
 export async function createReleaseDraft(
-    versionTag: string,
-    repoToken: string,
-    changeLog: string
+  versionTag: string,
+  repoToken: string,
+  changeLog: string
 ): Promise<string> {
-    const octokit = new gh.GitHub(repoToken)
+  const octokit = new gh.GitHub(repoToken)
 
-    const response = await octokit.repos.createRelease({
-        owner: gh.context.repo.owner,
-        repo: gh.context.repo.repo,
-        tag_name: versionTag,
-        name: version.removePrefix(versionTag),
-        body: markdown.toUnorderedList(changeLog),
-        prerelease: version.isPrerelease(versionTag),
-        draft: true
-    })
-    if (response.status != 201){
-        throw new Error('Failed to create release draft couscous: ${response.status}')
-    }
+  const response = await octokit.repos.createRelease({
+    owner: gh.context.repo.owner,
+    repo: gh.context.repo.repo,
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    tag_name: versionTag,
+    name: version.removePrefix(versionTag),
+    body: markdown.toUnorderedList(changeLog),
+    prerelease: version.isPrerelease(versionTag),
+    draft: true
+  })
+  if (response.status !== 201) {
+    throw new Error(
+      'Failed to create release draft couscous: ${response.status}'
+    )
+  }
 
-    core.info('Created successful release draft couscous ${response.data.name}')
-    return response.data.html_url
+  core.info('Created successful release draft couscous ${response.data.name}')
+  return response.data.html_url
 }
